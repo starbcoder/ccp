a545ec4eb401564a7e56e8ad3212666b76fbe0ee
buildwithbrainyl <brainyl.cloud@gmail.com>
2025-09-02 19:19:42 -0400

    Update ec2-userdata.sh

--- builders-day/s3/demo_s3/ec2-userdata.sh
+++ builders-day/s3/demo_s3/ec2-userdata.sh
@@ -4,6 +4,6 @@ sudo dnf -y install ruby
 sudo dnf -y install wget
 sudo dnf -y install httpd
 cd /home/ec2-user
-wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
